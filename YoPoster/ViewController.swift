473b8e050e7f82e6bd030737299752c2f90d27bf
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2023-07-15 13:40:13 +0800

    [feat] Add sharing methods

--- YoPoster/ViewController.swift
+++ YoPoster/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var shareBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var postTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+        setupBarButtonItems()
     }
-
-
+    
+    private func setupUI() {
+        title = "YoPoster"
+        postTextView.layer.cornerRadius = 12
+        postTextView.textContainerInset = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
+    }
+    
+    private func setupBarButtonItems() {
+        let image = UIImage(systemName: "heart.fill")?.withTintColor(.red)
+        let shareToInstagramAction = UIAction(
+            title: "Instagram",
+            image: nil,
+            handler: { [weak self] action in
+                let items: [Any] = [
+                    image,
+                ]
+                self?.share(with: items)
+            }
+        )
+        
+        let shareToTwitterAction = UIAction(
+            title: "Twitter",
+            image: nil,
+            handler: { [weak self] action in
+                let items: [Any] = [
+                    self?.postTextView.text,
+                    image,
+                ]
+                
+                self?.share(with: items)
+            }
+        )
+        
+        let menu = UIMenu(children: [shareToInstagramAction, shareToTwitterAction])
+        shareBarButtonItem.menu = menu
+    }
+    
+    private func share(with items: [Any]) {
+        let ac = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        ac.excludedActivityTypes = [.message, .mail]
+        present(ac, animated: true)
+    }
 }
-
