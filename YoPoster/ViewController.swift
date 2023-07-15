23f5e28d4c42c62a07ad4e31150a267b7853454a
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2023-07-15 14:09:02 +0800

    [feat] Add image picker

--- YoPoster/ViewController.swift
+++ YoPoster/ViewController.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 
 class ViewController: UIViewController {
     
+    // MARK: - Subviews
     @IBOutlet weak var shareBarButtonItem: UIBarButtonItem!
     @IBOutlet weak var postTextView: UITextView!
+    @IBOutlet weak var pickerImageView: UIImageView!
+    
+    // MARK: - Properties
+    private var image: UIImage? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,16 +30,19 @@ class ViewController: UIViewController {
         title = "YoPoster"
         postTextView.layer.cornerRadius = 12
         postTextView.textContainerInset = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
+        pickerImageView.layer.cornerRadius = 12
+        
+        let pickerImageViewTapped = UITapGestureRecognizer(target: self, action: #selector(pickerImageViewTapped))
+        pickerImageView.addGestureRecognizer(pickerImageViewTapped)
     }
     
     private func setupBarButtonItems() {
-        let image = UIImage(systemName: "heart.fill")?.withTintColor(.red)
         let shareToInstagramAction = UIAction(
             title: "Instagram",
             image: nil,
             handler: { [weak self] action in
                 let items: [Any] = [
-                    image,
+                    self?.image,
                 ]
                 self?.share(with: items)
             }
@@ -44,7 +54,7 @@ class ViewController: UIViewController {
             handler: { [weak self] action in
                 let items: [Any] = [
                     self?.postTextView.text,
-                    image,
+                    self?.image,
                 ]
                 
                 self?.share(with: items)
@@ -59,5 +69,35 @@ class ViewController: UIViewController {
         let ac = UIActivityViewController(activityItems: items, applicationActivities: nil)
         ac.excludedActivityTypes = [.message, .mail]
         present(ac, animated: true)
+    }
+    
+    @objc private func pickerImageViewTapped() {
+        showPhotoPicker()
+    }
+}
+
+// MARK: - Image Picker
+extension ViewController: PHPickerViewControllerDelegate {
+    
+    func showPhotoPicker() {
+        var config = PHPickerConfiguration(photoLibrary: .shared())
+        config.selectionLimit = 1
+        config.filter = .images
+        let pickerVC = PHPickerViewController(configuration: config)
+        pickerVC.delegate = self
+        present(pickerVC, animated: true)
+    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        results.first?.itemProvider.loadObject(ofClass: UIImage.self) { result, error in
+            guard let pickedImage = result as? UIImage, error == nil else { return }
+            DispatchQueue.main.async {
+                self.image = pickedImage
+                self.pickerImageView.contentMode = .scaleAspectFill
+                self.pickerImageView.image = pickedImage
+            }
+        }
     }
 }
